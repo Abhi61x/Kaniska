416bfd99e6635a315c2c5364732e9f5594f6b7ee
Abhi61x <abhi831830@gmail.com>
2026-01-20 12:09:23 +0530

    feat: Add Firebase Analytics
    Integrates Firebase Analytics into the Kaniska application to track user interactions and application usage. This includes updating the HTML and TypeScript configurations to include the analytics module and credentials.

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,23 +1,25 @@
-
 // @ts-ignore
 import { initializeApp } from 'firebase/app';
 // @ts-ignore
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 // @ts-ignore
 import { getFirestore } from 'firebase/firestore';
+// @ts-ignore
+import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB_npxvXYc39W7kFRLS3Fd61sTfivRhODA",
-  authDomain: "abhi-dashboard.firebaseapp.com",
-  databaseURL: "https://abhi-dashboard-default-rtdb.firebaseio.com",
-  projectId: "abhi-dashboard",
-  storageBucket: "abhi-dashboard.firebasestorage.app",
-  messagingSenderId: "370999830866",
-  appId: "1:370999830866:web:ce72f3e9835ac3cc84328b",
-  measurementId: "G-NSP8P7BP1M"
+  apiKey: "AIzaSyCBTHcih-7WjW72rA_AiLGSRVBCoohGV-4",
+  authDomain: "kaniska-f827d.firebaseapp.com",
+  databaseURL: "https://kaniska-f827d-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "kaniska-f827d",
+  storageBucket: "kaniska-f827d.firebasestorage.app",
+  messagingSenderId: "283233457469",
+  appId: "1:283233457469:web:fa03e4aa52385552b6e154",
+  measurementId: "G-DGLQPHJF9C"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider();
+export const analytics = getAnalytics(app);
